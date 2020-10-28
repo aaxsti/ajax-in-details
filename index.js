7d11426d878aca2c3011da39c8b6ce319c7bf36f
Maxim <66437371+aaxsti@users.noreply.github.com>
2020-10-28 21:59:34 +0300

    3. ui-dal (data access layer)

--- index.js
+++ index.js
@@ -2,17 +2,15 @@ const resultBlock = document.querySelector('#result');
 const pageNumberEl = document.querySelector('#page-number');
 const clickMeButton = document.querySelector('#click-me');
 
-clickMeButton.addEventListener('click', makeRequest);
+clickMeButton.addEventListener('click', () => {
+    getImages(pageNumberEl.value, onDataReceived);
+});
 
-function makeRequest() {
-    $.ajax(`https://repetitora.net/api/JS/Images?page=${pageNumberEl.value}&count=1`, {
-        success: function (data) {
-            data.forEach(el => {
-                const image = document.createElement('img');
-                image.src = el.thumbnail;
-                document.querySelector('#result').appendChild(image);
-            })
-        }
+function onDataReceived(data) {
+    data.forEach(el => {
+        const image = document.createElement('img');
+        image.src = el.thumbnail;
+        document.querySelector('#result').appendChild(image);
     });
 }
 
